bb6b81ef71a7d7711420ba0b59f15b0bde0c70db
EVGENY4510 <lentiay4510z@gmail.com>
2023-03-29 21:21:34 +0300

    hw-8,2.0

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,17 +2,20 @@ import throttle from 'lodash.throttle';
 const formEl = document.querySelector('.feedback-form');
 const textareaEl = document.querySelector('.feedback-form textarea ');
 const inputEl = document.querySelector('.feedback-form input');
-
+const btnEl = document.querySelector('.feedback-form button');
+btnEl.disabled = true;
 const feedback = {};
-
 const FEEDBACK_FORM = 'feedback-form-state';
 
 formEl.addEventListener('input', throttle(onFeedbackForm, 500));
 
 function onFeedbackForm(e) {
+  
   feedback[e.target.name] = e.target.value;
   localStorage.setItem(FEEDBACK_FORM, JSON.stringify(feedback));
+  btnEl.disabled = false;
 }
+
 onlocalStorage();
 
 formEl.addEventListener('submit', onSubmitForm);
@@ -20,19 +23,25 @@ formEl.addEventListener('submit', onSubmitForm);
 function onSubmitForm(e) {
   e.preventDefault();
 
-  console.log(feedback);
-  e.currentTarget.reset();
+  if (inputEl.value === '' || textareaEl.value === '') {
+    alert('not all fields are filled');
+  }
+  if (inputEl.value !== '' && textareaEl.value !== '') {
+    console.log(feedback);
+    e.currentTarget.reset();
+  }
+
+  
   localStorage.removeItem(FEEDBACK_FORM);
+  btnEl.disabled = true;
 }
 
 function onlocalStorage() {
   const saveFeedback = localStorage.getItem(FEEDBACK_FORM);
 
   if (saveFeedback) {
-    const { email, message } = JSON.parse(saveFeedback);
-    inputEl.value = email;
-      textareaEl.value = message;
-      feedback.email = email;
-      feedback.message = message;
+    const aaa = JSON.parse(saveFeedback);
+    inputEl.value = aaa.email;
+    textareaEl.value = aaa.message;
   }
 }
